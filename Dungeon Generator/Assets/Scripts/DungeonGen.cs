e7f835e3c6052f779af9d04c5b1ef1e5ab7b97b8
Andrey Mutlu <andrey.a.mutlu@gmail.com>
2026-05-23 16:54:25 +0200

    AddHorizontalSplit

--- Dungeon Generator/Assets/Scripts/DungeonGen.cs
+++ Dungeon Generator/Assets/Scripts/DungeonGen.cs
@@ -4,16 +4,27 @@ public class DungeonGenerator : MonoBehaviour
 {
     private RectInt _roomA;
     private RectInt _roomB;
+    
+    public bool splitHorizontally = false;
 
     private void Start()
     {
         var room = new RectInt(0, 0, 100, 50);
 
-        var splitX = room.width / 2;
-        
-        _roomA = new RectInt(room.x, room.y, splitX + 1, room.height);
-        
-        _roomB = new RectInt(room.x + splitX, room.y, room.width - splitX, room.height);
+        if (splitHorizontally)
+        {
+            var splitY = room.height / 2;
+            
+            _roomA = new RectInt(room.x, room.y, room.width, splitY + 1);
+            _roomB = new RectInt(room.x, room.y + splitY, room.width, room.height - splitY);
+        }
+        else
+        {
+            var splitX = room.width / 2;
+            
+            _roomA = new RectInt(room.x, room.y, splitX + 1, room.height);
+            _roomB = new RectInt(room.x + splitX, room.y, room.width - splitX, room.height);
+        }
     }
 
     private void Update()
